03ed1402382f37557e0847aaf76b0c72a36c6f6c
Pixolo <mrunali.pixolo@gmail.com>
2019-06-03 18:27:40 +0530

    css fixes

--- js/navigation.js
+++ js/navigation.js
@@ -15,7 +15,7 @@ var navigationservice = angular.module('navigationservice', [])
         //    var adminurl = "http://learnwithinq.com/adminpanel/testrest/index.php/";
         //HOME LAPTOP
         
-        var adminurl = "http://localhost/rest/index.php/";
+        var adminurl = "http://sunalisclasses.com/stgapi/rest/index.php/";
 
 
         var navigation = [{
